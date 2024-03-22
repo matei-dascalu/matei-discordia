ce8227b9acd8e3c2eee0766c368935906cd59c2c
Matei Dascalu <dascalumatei@gmail.com>
2024-03-23 00:09:13 +0200

    login page styling

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,10 +1,36 @@
 import "./Login.css";
-import LoginBackground from "./background/Background";
+// import LoginBackground from "./background/Background";
 
 export function Login() {
     return (
         <>
-            <LoginBackground />
+            <div className="background">
+                <div className="wrapper">
+                    <div className="card">
+                        <h1>Welcome to MateiDiscordia!</h1>
+                        <h2>We're so excited to see you again! </h2>
+                        <form>
+                            <div className="form-group">
+                                <label for="email">
+                                    EMAIL OR PHONE NUMBER <span> *</span>
+                                </label>
+                                <input type="email" id="email" name="email" required />
+                            </div>
+                            <div className="form-group">
+                                <label for="password">
+                                    PASSWORD <span> *</span>
+                                </label>
+                                <input type="password" id="password" name="password" required />
+                            </div>
+                            <a>Forgot your password?</a>
+                            <button type="submit">Log In</button>
+                        </form>
+                        <p>
+                            Need and account? <a href="#">Register</a>
+                        </p>
+                    </div>
+                </div>
+            </div>
         </>
     );
 }
--- src/pages/login/background/Background.jsx
+++
@@ -1,7 +0,0 @@
-import "./Background.css";
-
-function LoginBackground() {
-    return <div id="background"></div>;
-}
-
-export default LoginBackground;
